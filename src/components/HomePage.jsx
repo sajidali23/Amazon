28d8d8ab9d1f7e8ec04275ca3d05517763a24212
SAJID ALI <135090212+sajidali23@users.noreply.github.com>
2024-05-22 03:14:54 +0000

    Initial commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Carousel } from "./";
+const HomePage = () => {
+  return (
+    <div className="bg-amazonclone-background">
+      <div className="min-w-[1000px] max-w-[1500px] m-auto bg-purple-500">
+        testing
+        <Carousel />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
