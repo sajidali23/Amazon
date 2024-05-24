12252dbd11e944ddc364e5fc2be6cd7c7de96c16
SAJID ALI <135090212+sajidali23@users.noreply.github.com>
2024-05-24 02:31:08 +0000

    comited

---
+++ src/components/CarouselProduct.jsx
@@ -0,0 +1,25 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+const CarouselProduct = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={7}
+        spaceBetween={10}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {Array.from({ length: 9 }, (_, i) => (
+          <SwiperSlide key={i}>
+            <img src={`../images/product_${i}_small.jpg`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default CarouselProduct;
