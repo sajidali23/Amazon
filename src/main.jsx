714f41ad746076beed4d0f649cc3b1770de77a45
SAJID ALI <135090212+sajidali23@users.noreply.github.com>
2024-06-08 04:42:57 +0000

    comit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
